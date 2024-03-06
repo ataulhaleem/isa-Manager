8d061579914f9a824d5a96d94298ee2c7ba1d314
ataulhaleem <ataulhaleem@gmail.com>
2024-03-06 19:00:02 +0100

    nightmare use react-json-view-ssr not react-json-view

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,17 @@
-// "use client"
-
+"use client"
 import "./globals.css";
 import Header from "./components/Header";
-import {  Container } from '@mui/material';
+import { Container } from '@mui/material';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-      <Container maxWidth='ld'>
-        <Header />
-        {children}
-
+      <body>
+        <Container maxWidth='lg'>
+          <Header />
+          {children}
         </Container>
-        </body>
+      </body>
     </html>
   );
 }
-
-
--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    // <main className={styles.main}>
-    // </main>
+    <main className={styles.main}>
+    </main>
 
-    <>
-    </>
   );
 }
