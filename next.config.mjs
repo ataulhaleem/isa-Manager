c8c78ac8856e170c420d6abd07b5de50d4c62c7c
Haleem <ataulhaleem@gmail.com>
2024-03-12 19:35:55 +0100

    Update next.config.mjs
    added a '/' before basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    basePath: 'isa-Manager',
+    basePath: '/isa-Manager',
     reactStrictMode : true
   };
   /* module.exports = nextConfig*/
