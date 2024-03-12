3af0bbc8879cac49f83df3323b40aa820ea9ef91
ataulhaleem <ataulhaleem@gmail.com>
2024-03-12 16:37:58 +0100

    next.config.mjs default settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    target: 'experimental-serverless-trace',
-    output:'browser',
-  };
-  /* module.exports = nextConfig*/
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
